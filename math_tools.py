0d2a19b340eaccad8e026c4c2b725623ec845b35
ryzquo <ryzquo@gmail.com>
2024-11-01 13:53:30 +0800

    添加一维卷积

--- math_tools.py
+++ math_tools.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# 数学计算相关工具
 
 import math
+
+
+def conv_1d(x, kernel):
+    """
+    一维卷积
+    :param x: 一维数据
+    :param kernel: 卷积核
+    :return:
+    """
+
 
 
 def angle2radian(angle):
